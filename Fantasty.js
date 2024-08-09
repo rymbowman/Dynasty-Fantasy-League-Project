096d24d68fa3b7f07802244da4702ce7623ad413
Ryan Bowman <[email protected]>
2024-08-09 12:17:19 -0400

    first commit

---
+++ Fantasty.js
@@ -0,0 +1,23 @@
+
+let button = document.querySelector('#btn');
+let sideBar = document.querySelector('.sidebar');
+
+btn.onclick = function() {
+    sideBar.classList.toggle('active');
+}
+
+/*let accordionButton = document.querySelector('.pr-accordion');
+let accordionPanel = document.querySelector('.panel');
+
+btn.onclick = function() {
+    accordionPanel.classList.toggle('active');
+}*/
+
+
+
+//below are the functions to activate and deactivate the modals on my members page
+
+
+
+
+
